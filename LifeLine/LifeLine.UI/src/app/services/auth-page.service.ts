ef8f396ddc65f58ec0a4a3eda34e195e60c67aaa
Muntean Mihai <mihai.muntean@isa.utm.md>
2023-09-15 20:32:28 +0300

    wrapper: Services

---
+++ LifeLine/LifeLine.UI/src/app/services/auth-page.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthPageService {
+
+  private baseUrl: string = ""
+  constructor(private http: HttpClient) { }
+
+  singUp(userInfo: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/register`, userInfo);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/login`, credentials);
+  }
+
+}
