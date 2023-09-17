91eeaa4bafa8c88aa3548da508aac852341e578d
Muntean Mihai <104444316+MunMihai@users.noreply.github.com>
2023-09-17 20:06:43 +0300

    Wrapper (#4)
    * wrapper: Services

--- LifeLine/LifeLine.UI/src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ LifeLine/LifeLine.UI/src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthPageService } from 'src/app/services/auth-page.service';
 
 @Component({
   selector: 'app-auth-page',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth-page.component.css']
 })
 export class AuthPageComponent {
+
+  showLogin: boolean = true;
+
+  constructor(private authServices: AuthPageService, private router: Router) { }
+
+  onLogin(user: any) {
+    this.authServices.login(user).subscribe(
+      (response) => {
+        console.log(`Logged: ${user}`);
+      },
+      (error) => {
+        console.error('Login Error: ',error)
+      },
+      ()=>{
+        alert('Login Execued!');
+      }
+    );
+  }
+
+  onRegister(user: any) {
+    this.authServices.singUp(user).subscribe(
+        (response) => {
+          console.log(`Registerde: ${user}`);
+        },
+        (error) => {
+          console.error('Register Error: ',error)
+        },
+        ()=>{
+        alert('Register Execued!');
+        }
+    );
+  }
 
 }
